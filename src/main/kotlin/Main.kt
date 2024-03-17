95865b5e0a0bb7465e857ed833bca6ebe6f824b0
yoprogramo <saul.riglez@gmail.com>
2024-03-17 15:23:01 -0400

    - Converting Monomorphic to polymorphic function.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package org.example
 
 fun main() {
-    val stringList = mutableListOf(
+    val array1 = arrayOf(
         "yoprogramo",
         "saul",
         "rios",
@@ -9,17 +9,21 @@ fun main() {
         "areli"
     )
 
-    println(findString(stringList, "Jessy"))
+    val array2 = arrayOf(1, 34, 45, 500, 45, 34)
+    val predicate1: (String) -> Boolean = { it == "rios" }
+    val predicate2: (Int) -> Boolean = { it == 500 }
+
+    println(findFirst(array1, predicate1))
+    println(findFirst(array2, predicate2))
 }
 
-fun findString(stringList: MutableList<String>, key: String): Int {
+fun <T> findFirst(xs: Array<T>, p: (T) -> Boolean): Int {
     tailrec fun loop(n: Int): Int {
         return when {
-            n >= stringList.size -> -1
-            stringList[n] == key -> n
+            n >= xs.size -> -1
+            p.invoke(xs[n]) -> n
             else -> loop(n + 1)
         }
     }
-
     return loop(0)
 }
