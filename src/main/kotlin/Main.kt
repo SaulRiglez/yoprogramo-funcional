1cc50c1d870c55d93d4133fa051c128269eff653
yoprogramo <saul.riglez@gmail.com>
2024-03-16 23:40:28 -0400

    Find string in list recursive.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,25 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    val stringList = mutableListOf(
+        "yoprogramo",
+        "saul",
+        "rios",
+        "Jessy",
+        "areli"
+    )
+
+    println(findString(stringList, "Jessy"))
+}
+
+fun findString(stringList: MutableList<String>, key: String): Int {
+    tailrec fun loop(n: Int): Int {
+        return when {
+            n >= stringList.size -> -1
+            stringList[n] == key -> n
+            else -> loop(n + 1)
+        }
+    }
+
+    return loop(0)
 }
